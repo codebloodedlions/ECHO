71137c56e491d6372e71ac73a1538d34f005374c
deonnao <deonnaowens3@yahoo.com>
2023-04-02 04:18:50 -0500

    updated the events ui

--- app/src/main/java/com/example/echo/UserInterface/EventsActivity.java
+++ app/src/main/java/com/example/echo/UserInterface/EventsActivity.java
@@ -22,34 +22,25 @@ public class EventsActivity extends AppCompatActivity {
     RecyclerView eventsRV;
     EventsAdapter eventsAdapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList<Event> eventArrayList = new ArrayList<>();
+    ArrayList<Object> eventArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        List<Object> threadedDataList = Collections.synchronizedList(new ArrayList<Object>());
+        List<Event> threadedDataList = Collections.synchronizedList(new ArrayList<Event>());
 
         Thread test = new Thread(() -> {
             AssetManager assetManager = getAssets();
             String[] temp = EventGrabber.getEventData(assetManager).get(2);
 
-            threadedDataList.addAll(Arrays.asList(temp));
+            //threadedDataList.addAll(Arrays.asList(temp));
 
+            Log.i("TEMP", Arrays.toString(temp));
             Log.i("PARCEL", threadedDataList.toString());
         });
         test.start();
-
-
-        //eventArrayList.addAll(threadedDataList);
-        eventsRV = findViewById(R.id.eventsRV);
-        eventsRV.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        eventsRV.setLayoutManager(layoutManager);
-        eventsAdapter = new EventsAdapter(this, eventArrayList, eventsRV);
-        eventsRV.setAdapter(eventsAdapter);
-
 
     }
 }
