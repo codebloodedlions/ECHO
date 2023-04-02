569ed9137ebc37dcb14eb9ef7ba6195265b6566b
deonnao <deonnaowens3@yahoo.com>
2023-04-02 05:35:41 -0500

    updated the events ui

--- app/src/main/java/com/example/echo/UserInterface/EventsActivity.java
+++ app/src/main/java/com/example/echo/UserInterface/EventsActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.echo.R;
 import com.example.echo.api.EventGrabber;
@@ -29,15 +31,20 @@ public class EventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        List<Event> threadedDataList = Collections.synchronizedList(new ArrayList<Event>());
+        ImageView event = findViewById(R.id.image4);
+
+        event.setOnClickListener(view -> {
+            Intent intent = new Intent(this, EventDetail.class);
+            startActivity(intent);
+        });
+
+        List<Object> threadedDataList = Collections.synchronizedList(new ArrayList<Object>());
 
         Thread test = new Thread(() -> {
             AssetManager assetManager = getAssets();
             String[] temp = EventGrabber.getEventData(assetManager).get(2);
 
-            //threadedDataList.addAll(Arrays.asList(temp));
-
-            Log.i("TEMP", Arrays.toString(temp));
+            threadedDataList.addAll(Arrays.asList(temp));
             Log.i("PARCEL", threadedDataList.toString());
         });
         test.start();
