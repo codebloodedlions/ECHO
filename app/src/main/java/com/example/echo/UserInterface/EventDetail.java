e8a45500e92dcd0b05bd3558fcf82c708291834c
deonnao <deonnaowens3@yahoo.com>
2023-04-01 21:20:03 -0500

    Implemented UI for login and landing page

---
+++ app/src/main/java/com/example/echo/UserInterface/EventDetail.java
@@ -0,0 +1,16 @@
+package com.example.echo.UserInterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.echo.R;
+
+public class EventDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+    }
+}
